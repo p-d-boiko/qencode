f595cfddfa6a30e13332a0491a05e2a59118a182
Pavlo Boiko <pavlo@boiko.work>
2024-03-19 15:17:29 +0200

    add set-password route, view

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,6 +4,7 @@ import Layout from 'client:views/layout'
 import ErrorView from 'client:views/error'
 import LoginView from 'client:views/login'
 import ResetPasswordView from 'client:views/reset-password'
+import SetNewPasswordView from 'client:views/set-new-password'
 
 import { ROUTES, MISSING_RESOURCE_MESSAGE } from './constants'
 import { getSlug } from './utils'
@@ -24,6 +25,20 @@ export const routes: RouteObject[] = [
       {
         path: getSlug(ROUTES.SignUp),
         element: <p>Sign Up</p>,
+      },
+      {
+        path: getSlug(ROUTES.SetNewPassoword, { positionInUri: 0 }),
+        children: [
+          {
+            path: getSlug(ROUTES.SetNewPassoword, { positionInUri: 1 }),
+            Component: SetNewPasswordView,
+          },
+          {
+            index: true,
+            path: '*',
+            element: <ErrorView code={404} message={MISSING_RESOURCE_MESSAGE} />,
+          },
+        ],
       },
       {
         index: true,
